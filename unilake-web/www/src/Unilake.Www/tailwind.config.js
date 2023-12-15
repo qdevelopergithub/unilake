25cbaa34a0e1849eb404d8d74230f39521f68869
Gurjeet Singh <43872892+qdevelopergithub@users.noreply.github.com>
2023-12-15 17:49:09 +0530

    header design done

--- unilake-web/www/src/Unilake.Www/tailwind.config.js
+++ unilake-web/www/src/Unilake.Www/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
             'lg': '1024px',
             'xl': '1280px',
             '2xl': '1440px',
+            '3xl':'1920px'
         },
         colors: {
             backgroundSecondary: '#121212',
